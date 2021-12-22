5cf4820978acf808ffec06eecf034f889351ef6e
Christian Garcia <lgnaitsirhc20@gmail.com>
2021-12-22 23:35:49 +0000

    1st Version

---
+++ code/skyline.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "skylib.h"
+
+int main(){
+    int nbuilds = 0;
+    int allB[5000][3] = {0};
+    int maxBuild = 0;
+
+    nbuilds = inputBuild(allB);
+    printf("%d, %d", allB[i][0],allB[i][1]);
+
+    for (int i = 1; i < nbuilds; i++)
+    {
+        
+    }
+    return 0;
+}
