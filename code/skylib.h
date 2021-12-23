2d7c8ab2bcacd7d79a4a5194a089041aadd4bd0e
Christian Garcia <lgnaitsirhc20@gmail.com>
2021-12-23 20:12:01 +0000

    2nd Version

--- code/skylib.h
+++ code/skylib.h
@@ -1,58 +1,104 @@
 #include <stdio.h>
 
 int inputBuild(int allB[][3]); //function to fill array with data according to rules
-void gapMax(int ultBuild, int iBuild, int maxBuild, int allB[][3]);
+int searchlastB(int lastB, int currB, int allB[][3]);
+int buildCollision(int lastB, int iBuild, int currB, int allB[][3]); //analyse if highest building considered collides with building number i
+int gap(int lastB, int iBuild, int currB, int allB[][3], int end);   //analyse buildings between highest building considered and building number i
 
 int inputBuild(int allB[][3])
 {
+    int i = 0;
     int newBuild = 1;
-    for (int i = 0; i < 5000 && newBuild == 1; i++) //maximum of 5000 buildings, ending there or when user decides to stop
+    for (i = 0; i < 5000 && newBuild == 1; i++) //maximum of 5000 buildings, ending there or when user decides to stop
     {
+        //"while" with set of rules to be considered when inserting building data
         do
         {
             printf("\nInsert building data (Li, Hi, Ri): ");
             scanf("%d %d %d", &allB[i][0], &allB[i][1], &allB[i][2]);
         } while (allB[i][0] >= 10000 || allB[i][1] >= 10000 || allB[i][1] <= 0 || allB[i][2] >= 10000 || allB[i][0] >= allB[i][2] || allB[i][0] <= i || (i != 0 && allB[i][0] <= allB[i - 1][0]));
-        //"while" with set of rules to be considered when inserting building data
+
+        //query to stop adding buildings
         do
         {
             printf("\nInsert 1 to add new building, 0 to end input: ");
             scanf("%d", &newBuild);
         } while (newBuild != 0 && newBuild != 1);
-        //query to stop adding buildings
     }
     return i; //return number of buildings
 }
 
-void gapMax(int ultBuild, int iBuild, int maxBuild, int allB[][3])
+int searchlastB(int lastB, int currB, int allB[][3])
 {
-    if (allB[maxBuild][2] < allB[iBuild][0])
+    int stop = 0;
+    for (int i = lastB; i <= currB && stop == 0; i++)
     {
-        int max = -1;
-        for (int i = ultBuild; i < iBuild; i++)
+        if (allB[i][2] > allB[currB][0])
         {
-            if (i != maxBuild)
+            stop = 1;
+            lastB = i;
+        }
+    }
+    return lastB;
+}
+
+int buildCollision(int lastB, int iBuild, int currB, int allB[][3])
+{
+    if (allB[currB][2] >= allB[iBuild][0])
+    {
+        if (allB[currB][1] < allB[iBuild][1])
+        {
+            printf(", %d, %d", allB[iBuild][0], allB[iBuild][1]);
+            currB = iBuild;
+        }
+        if (allB[currB][1] == allB[iBuild][1])
+        {
+            currB = iBuild;
+        }
+    }
+    else
+    {
+        currB = gap(lastB, iBuild, currB, allB, 0);
+    }
+    return currB;
+}
+
+int gap(int lastB, int iBuild, int currB, int allB[][3], int end)
+{
+    int max = -1;
+    for (int i = lastB; i < iBuild; i++)
+    {
+        if (i != currB)
+        {
+            if (allB[i][0] <= allB[currB][2] && allB[i][2] > allB[currB][2])
             {
-                if (allB[i][0] <= allB[maxBuild][2] && allB[i][2] > allB[maxBuild][2])
+                if (allB[i][1] > allB[max][1] || max == -1)
                 {
-                    if (allB[i][1] > allB[max][1] || max == -1)
-                    {
-                        max = i;
-                    }
+                    max = i;
                 }
             }
         }
-        if (max != -1)
+    }
+    if (max != -1)
+    {
+        printf(", %d, %d", allB[currB][2], allB[max][1]);
+        if (end == 0)
         {
-            printf(", %d, %d", allB[maxBuild][2], allB[max][1]);
-            gapMax(ultBuild, iBuild, max, allB);
+            currB = buildCollision(lastB, iBuild, max, allB);
         }
         else
         {
-            printf(", %d, %d", allB[maxBuild][2], 0);
+            currB = gap(lastB, iBuild, max, allB, 1);
         }
     }
-    else{
-
+    else
+    {
+        printf(", %d, %d", allB[currB][2], 0);
+        if (end == 0)
+        {
+            currB = iBuild;
+            printf(", %d, %d", allB[currB][0], allB[currB][1]);
+        }
     }
+    return currB;
 }
--- code/skyline.c
+++ code/skyline.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include "skylib.h"
 
-int main(){
+int main()
+{
     int nbuilds = 0;
+    int currB = 0;
+    int lastB = 0;
     int allB[5000][3] = {0};
-    int maxBuild = 0;
+    int i = 0;
 
     nbuilds = inputBuild(allB);
-    printf("%d, %d", allB[i][0],allB[i][1]);
+    printf("%d, %d", allB[0][0], allB[0][1]);
 
-    for (int i = 1; i < nbuilds; i++)
+    for (i = 1; i < nbuilds; i++)
     {
-        
+        lastB = searchlastB(lastB, currB, allB);
+        currB = buildCollision(lastB, i, currB, allB);
     }
+    currB = gap(lastB, i, currB, allB, 1);
     return 0;
 }
